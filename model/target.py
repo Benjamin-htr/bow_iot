2f1d2d5a6248f48054f9df34ff47a07f6057155f
MartyHughes <marty.hughes@student.junia.com>
2023-11-02 16:05:20 +0100

    upadate hitbox taget

--- model/target.py
+++ model/target.py
@@ -1,18 +1,22 @@
 class Target:
   def __init__(self):
-    self.x = 0
     self.y = 50
     self.direction = 1
+    self.hit_box_lower = 30 
+    self.hit_box_upper = 70
   
   def move(self):
-    self.x += self.direction
-    if self.x == 100 or self.x == 0:
+    self.y += self.direction
+    if self.y == 100 or self.y == 0:
       self.direction *= -1
+    self.hit_box_lower = self.y - 20 # evolution de la hit box en fonction de la position de la cible
+    self.hit_box_upper = self.y + 20
   
   def get_score(self, arrow_x, arrow_y):
-    #formule de calcul de la distance entre deux points dans un environnement 2D avec Euclidean
-    distance = ((arrow_x - self.x) ** 2 + (arrow_y - self.y) ** 2) ** 0.5
-    #exemple
+  
+    # calule la distance entre la cible et la fleche en utilisant la fonction eclidian
+    distance = self.y - self.arrow_y
+    # calcule le score en fonction de la distance
     if distance <= 10:
       return 3
     elif distance <= 20:
@@ -21,3 +25,6 @@ class Target:
       return 1
     else:
       return 0
+  
+  def target_position_y(self):
+    return self.hit_box_lower
