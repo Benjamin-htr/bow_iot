ec87e3d6881eb8e740d507cda37604dada8b93b2
leooz <leo59coc@gmail.com>
2023-11-02 17:24:02 +0100

    cool

---
+++ model/Target.py
@@ -0,0 +1,8 @@
+class Target:
+    def __init__(self, position, size):
+        self.position = position
+        self.size = size
+
+    def is_hit(self, arrow_position):
+        # Déterminer si la flèche a atteint la cible
+        pass
