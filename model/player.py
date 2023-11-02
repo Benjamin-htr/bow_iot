2a98b1b6dafe1b49a730faa157cff84c0c337042
leooz <leo59coc@gmail.com>
2023-11-02 14:27:18 +0100

    init

---
+++ model/player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, rfid_tag, name):
+        self.rfid_tag = rfid_tag
+        self.name = name
+        self.score = 0
+
+    def update_score(self, points):
+        self.score += points
