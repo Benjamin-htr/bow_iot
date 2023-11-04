2badaf9d5ea679e543352b2eedf4ae20dcefeaac
Benjamin Hautier <benjamin.htr42@gmail.com>
2023-11-04 17:28:21 +0100

    feat: add assets + dummy animation

--- view/game.py
+++ view/game.py
@@ -2,14 +2,14 @@ import math
 
 import arcade
 from gameParts.bowSprite import BowSprite
+from gameParts.dummySprite import DummySprite
 from gameParts.powerIndicator import PowerIndicator
 
-BOW_SCALING = 1
+BOW_SCALING = 1.5
+DUMMY_SCALING = 2
 
-UPDATES_PER_FRAME = 3
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN_HEIGHT = 619
 
 ANGLE_SPEED = 5
 BANDAGE_SPEED = 1
@@ -18,9 +18,11 @@ BANDAGE_SPEED = 1
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
-        # Set up the bow
+
         self.bow = None
+        self.dummy = None
         self.power_indicator = None
+        self.background = None
 
         self.score = 0
 
@@ -28,26 +30,41 @@ class GameView(arcade.View):
         # Set up the bow
         self.bow = BowSprite(BOW_SCALING)
 
-        # Set up the power indicator
-        self.power_indicator = PowerIndicator(SCREEN_WIDTH - 50, SCREEN_HEIGHT // 2, 20, 100, 5)
+        #Set up the dummy
+        self.dummy = DummySprite(DUMMY_SCALING)
 
-        self.bow.center_x = 50
-        self.bow.center_y = SCREEN_HEIGHT // 2
+        # Set up the power indicator
+        self.power_indicator = PowerIndicator(SCREEN_WIDTH - 50, 100, 20, 100, 5)
+
+        # Set up the background
+        self.background = arcade.load_texture("assets/background.png")
+
+        self.bow.center_x = 60
+        self.bow.center_y = SCREEN_HEIGHT // 3.5
+
+        self.dummy.center_x = SCREEN_WIDTH - 150
+        self.dummy.center_y = SCREEN_HEIGHT // 3.5
 
 
 
     def on_show_view(self):
-        arcade.set_background_color(arcade.color.VIOLET_BLUE)
         return
 
     def on_draw(self):
         self.clear()
 
+        # Draw the background texture
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
          # Draw all the sprites.
         self.bow.draw()
 
+        # Draw the dummy
+        self.dummy.draw()
+
         # Draw power indicator bar
         self.power_indicator.draw()
+
 
          # Put the text on the screen.
         output = f"Score: {self.score}"
@@ -68,11 +85,14 @@ class GameView(arcade.View):
         elif key == arcade.key.RIGHT:
             self.bow.change_angle = -ANGLE_SPEED
 
+        self.dummy.hitted = False
+
 
     def on_key_release(self, key, modifiers):
         #when the key is released, the arrow is shot
         if key == arcade.key.SPACE:
             self.bow.change_power = 0
+            self.dummy.hitted = True
 
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.bow.change_angle = 0
@@ -87,6 +107,9 @@ class GameView(arcade.View):
 
         # Update the power indicator
         self.power_indicator.update(self.bow.power)
+
+        # Update the dummy animation
+        self.dummy.update_animation()
 
 
         print("bandage : ", self.bow.power, " angle :", self.bow.angle)
