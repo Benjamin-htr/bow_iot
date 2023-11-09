54be68f253b3a41c6cc6db18b5a950dc6cb99573
hugoD4846 <dutoit.hugo2001@gmail.com>
2023-11-09 13:45:33 +0000

    ult card

--- mosquitto/controller.py
+++ mosquitto/controller.py
@@ -7,9 +7,12 @@ class mqtt_observer:
     onButton2Pressed = None
     onButton2Released = None
     onPotarChanged = None
-
+    setRfid = None
+    onUltChange = None
+    def sendVictory():
+        client.publish("AP5iot/victory", "youhou",2)
     def on_mes(self, client, userdata, mes):
-        msg = int(mes.payload.decode("utf-8"))
+        msg = mes.payload.decode("utf-8")
         if mes.topic == "AP5iot/btn/1":
             if msg and self.onButton1Pressed:
                 self.onButton1Pressed()
@@ -22,7 +25,13 @@ class mqtt_observer:
                 self.onButton2Released()
         elif mes.topic == "AP5iot/potar":
             if self.onPotarChanged:
-                self.onPotarChanged(msg)
+                self.onPotarChanged(int(msg))
+        elif mes.topic == "AP5iot/ult":
+            if self.onUltChanged:
+                self.onUltChanged(int(msg))
+        elif mes.topic == "AP5iot/card":
+            if self.setRfid:
+                self.setRfid(msg)
 
     def __init__(
         self,
@@ -31,15 +40,19 @@ class mqtt_observer:
         onButton2Pressed=None,
         onButton2Released=None,
         onPotarChanged=None,
+        setRfid = None,
+        onUltChange = None
     ):
         self.onButton1Pressed = onButton1Pressed
         self.onButton1Released = onButton1Released
         self.onButton2Pressed = onButton2Pressed
         self.onButton2Released = onButton2Released
         self.onPotarChanged = onPotarChanged
+        self.setRfid = setRfid
+        self.onUltChange = onUltChange
         self.client = mqtt.Client()
         self.client.on_message = self.on_mes
-        self.client.connect("10.224.0.211", 1883)
+        self.client.connect("192.168.1.102", 1883)
         self.client.subscribe("AP5iot/#", 2)
         self.client.loop_start()
 
